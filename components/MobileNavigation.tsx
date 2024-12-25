3b32c4c6f728dbffe877ce1efa8a454ca824e159
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-12-25 15:32:16 +0530

    file upload completed

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -17,16 +17,17 @@ import { Separator } from "./ui/separator";
 import { navItems } from "@/constants";
 import FileUploader from "./FileUploader";
 import { signOutUser } from "@/lib/actions/user.actions";
+import { Button } from "./ui/button";
 
 interface Props {
-  ownerId: string;
+  $id: string;
   accountId: string;
   fullName: string;
   avatar: string;
   email: string;
 }
 
-const MobileNavigation = ({ ownerId, accountId, fullName, avatar, email }: Props) => {
+const MobileNavigation = ({ $id: ownerId, accountId, fullName, avatar, email }: Props) => {
 
   const [open, setOpen] = useState(false);
   const pathname = usePathname();
@@ -38,37 +39,41 @@ const MobileNavigation = ({ ownerId, accountId, fullName, avatar, email }: Props
         <h1 className="font-poppins font-[500] text-2xl leading-9 text-brand"><span className="font-semibold text-4xl">C</span>loud<span className="font-semibold text-4xl">H</span>aven</h1>
       </div>
       <Sheet open={open} onOpenChange={setOpen}>
-        <SheetTrigger><Menu size={35} color="#FA7275" /></SheetTrigger>
+        <SheetTrigger>
+          <Image src="/assets/icons/menu.svg" alt="Search" width={30} height={30} />
+        </SheetTrigger>
         <SheetContent className="shad-sheet h-screen px-3">
-          <SheetTitle className="header-user">
-            <Image src={avatar} alt="avatar" height={44} width={44} className="header-user-avatar" />
-            <div className="sm:hidden lg:block">
-              <p className="subtitle-2 capitalize">{fullName}</p>
-              <p className="caption">{email}</p>
+          <SheetTitle>
+            <div className="header-user">
+              <Image src={avatar}
+                alt="avatar" width={44} height={44} className="header-user-avatar" />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
             </div>
+            <Separator className="mb-4 bg-light-200/20" />
           </SheetTitle>
-          <div className="pb-4"><Separator className="bg-light-200/20" /></div>
-          <SheetDescription>
-            <nav className="mobile-nav">
-              <ul className="mobile-nav-list">
-                {navItems.map(({ url, name, icon }) => (
-                  <Link key={name} href={url} className='lg:w-full'>
-                    <li className={cn("mobile-nav-item", pathname === url && "shad-active",)}>
-                      <Image src={icon} alt='name' height={24} width={24} className={cn('nav-icon', pathname === url && "nav-icon-active")} />
-                      <p>{name}</p>
-                    </li>
-                  </Link>
-                ))}
-              </ul>
-            </nav>
-          </SheetDescription>
-          <div className="my-4"><Separator className="bg-light-200/20" /></div>
+
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              {navItems.map(({ url, name, icon }) => (
+                <Link key={name} href={url} className="lg:w-full">
+                  <li className={cn("mobile-nav-item", pathname === url && "shad-active",)}>
+                    <Image src={icon} alt={name} width={24} height={24} className={cn("nav-icon", pathname === url && "nav-icon-active",)} />
+                    <p>{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <Separator className="my-5 bg-light-200/20" />
           <div className="flex flex-col justify-between gap-5 pb-5">
-            <FileUploader />
-            <button type='submit' className='mobile-sign-out-button' onClick={async()=>await signOutUser()}>
-              <Image src="/assets/icons/logout.svg" alt="logo" width={24} height={24}/>              
+            <FileUploader ownerId={ownerId} accountId={accountId} />
+            <Button type="submit" className="mobile-sign-out-button" onClick={async () => await signOutUser()}            >
+              <Image src="/assets/icons/logout.svg" alt="logo" width={24} height={24} />
               <p>Logout</p>
-            </button>
+            </Button>
           </div>
         </SheetContent>
       </Sheet>
